90d5bc8ab9bf5aa1f0b0b7798421a578625d356d
Raiff-85 <emanoel.gomes@dcx.ufpb.br>
2024-04-27 17:18:32 -0300

    modoficacao do HomoeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,10 +10,19 @@ class HomeController extends AbstractController
 {
 
     #[Route('/')]
-    public function new(): Response
+    public function index(): Response
     {
         $pageTitle = "Sistema de Notícias";
         return $this->render('home.html.twig', [
+            'pageTitle' => $pageTitle,
+        ]);
+    }
+
+    #[Route('/categoria')]
+    public function categoria(): Response
+    {
+        $pageTitle = "Notícias";
+        return $this->render('categoria.html.twig', [
             'pageTitle' => $pageTitle,
         ]);
     }
